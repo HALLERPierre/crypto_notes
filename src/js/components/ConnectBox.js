fd775fa088e58bb7ab43f5e37d021916b15f2653
Maxime <richard.maxime9@gmail.com>
2017-07-01 15:14:23 +0200

    review ConnectBox

--- src/js/components/ConnectBox.js
+++ src/js/components/ConnectBox.js
@@ -1,32 +1,45 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 class ConnectBox extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            user: '',
-            password: ''
-        }
+
+    static propTypes = {
+        disabled: PropTypes.bool,
+        connect: PropTypes.func.isRequired,
+    }
+
+    static defaultProps = {
+        disabled: false,
+    }
+
+    state = {
+        user: '',
+        password: '',
+    }
+
+    handleConnect = () => {
+        this.props.connect(this.state.user, this.state.password);
+    }
+
+    updateUsername = (event) => {
+        this.setState({ user: event.target.value });
+    }
+
+    updatePassword = (event) => {
+        this.setState({ password: event.target.value });
     }
 
     render() {
-        const updateUsername = (event) => {
-            this.setState({user: event.target.value})
-        }
-        const updatePassword = (event) => {
-            this.setState({password: event.target.value})
-        }
-
         return (
-    		<div className="Welcoming">
-    			<h2>Welcome on crypto-notes !</h2>
-    			<label>Username </label>: <input className="text" value={this.state.user} onChange={updateUsername}/>
-    			<br/>
-    			<label>Password </label>: <input className="text" value={this.state.password} onChange={updatePassword}/>
-    			<h2>Click here to begin : <button disabled={this.props.disabled} onClick={() => this.props.connect(this.state.user, this.state.password)}>CONNECT</button></h2>
-    		</div>
-        )
+            <div className="Welcoming">
+                <h2>Welcome on crypto-notes !</h2>
+                <label>Username </label>: <input className="text" value={this.state.user} onChange={this.updateUsername} />
+                <br />
+                <label>Password </label>: <input className="text" value={this.state.password} onChange={this.updatePassword} />
+                <h2>Click here to begin : <button disabled={this.props.disabled} onClick={this.handleConnect}>CONNECT</button></h2>
+            </div>
+        );
     }
 }
 
-export default ConnectBox
+export default ConnectBox;
