fff574649928b4186c769c1aac83fee0af9a9c9a
Maxime <richard.maxime9@gmail.com>
2017-07-12 00:32:01 +0200

    add style add note layout

--- src/js/layout/Layout.js
+++ src/js/layout/Layout.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { Notes } from '../containers';
 
 const style = {
+    container: {
+        padding: "0 10%"
+    },
     header: {
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
         alignContent: "center",
-        background: "#D7737A",
-        padding: "0 10%"
+        background: "#D7737A"
     },
     title: {
         color: "#302D30"
@@ -17,6 +19,31 @@ const style = {
         border: "none",
         height: "24px",
         padding: "11px 0 11px 16px"
+    },
+    newNote: {
+        background: "#e8e8e8",
+        paddingTop: "1.5em",
+        paddingBottom: "1em"
+    },
+    takeNote: {
+        display: "block",
+        margin: "0 auto",
+        maxWidth: "35em",
+        height: "1.5em",
+        border: "none",
+        width: "100%",
+        padding: ".5em"
+    },
+    buttonAddNote: {
+        border: "none",
+        display: "block",
+        margin: "1em auto 0",
+        padding: ".35em 1em",
+        textTransform: "uppercase",
+        background: "#D7737A",
+        color: "#FFFFFF",
+        fontSize: "14px",
+        cursor: "pointer"
     }
 }
 
@@ -27,7 +54,7 @@ const DefaultLayout = ({ ...props }) => (
 );
 
 const HeaderLayout = ({ ...props }) => (
-    <header style={style.header}>
+    <header style={{...style.container, ...style.header}}>
         <h1 style={style.title}>Crypto notes</h1>
         <form style={style.search} method="get">
             <input type="search" name="search" placeholder="Search" />
@@ -36,9 +63,9 @@ const HeaderLayout = ({ ...props }) => (
 );
 
 const NewNoteLayout = ({ ...props }) => (
-    <div>
-        <textarea name="text" placeholder="Take a note..."></textarea>
-        <button>Done</button>
+    <div style={{...style.container, ...style.newNote}}>
+        <textarea style={style.takeNote} name="text" placeholder="Take a note..."></textarea>
+        <button style={style.buttonAddNote}>Done</button>
     </div>
 );
 
